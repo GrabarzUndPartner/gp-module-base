8e890fd34cbf0e35a23678ac60d712b9681fec8b
Thorn Walli <thorn.walli@grabarzundpartner.de>
2017-06-29 11:27:50 +0200

    clean DomModel

--- src/DomModel.js
+++ src/DomModel.js
@@ -7,12 +7,5 @@ export default AmpersandModel.extend(dataTypeDefinition, {
 
     initialize: function() {
         AmpersandModel.prototype.initialize.apply(this, arguments);
-
-        if(module.hot) {
-            module.hot.addDisposeHandler(function(data) {
-                console.log('WHAT', arguments, this.firstName, this.toJSON());
-                data.firstName = this.firstName;
-            }.bind(this));
-        }
     }
 });
